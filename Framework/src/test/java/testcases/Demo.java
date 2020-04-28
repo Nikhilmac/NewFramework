20551ad0148d6a2f7c98f4b2067f6eb2c0224334
Nikhilmac <nikhil.qa124@@gmail.com>
2020-04-29 00:43:48 +0530

    Code Changes

---
+++ Framework/src/test/java/testcases/Demo.java
@@ -0,0 +1,42 @@
+package testcases;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class Demo {
+	WebDriver driver;
+
+	@Test
+	public void f() {
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+		String name = driver.getTitle();
+		System.out.println(name);
+		driver.findElement(By.linkText("Registration"));
+
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		System.setProperty("webdriver.chrome.silentOutput", "true");
+		System.setProperty("webdriver.chrome.driver", "E:\\selenium\\Chrome 80.3987.106\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://www.way2automation.com/demo.html");
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+	}
+
+	@AfterTest
+	public void afterTest() {
+
+	}
+
+}
